e01d96a2080f4913335074af931195a600903c31
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-09-13 18:34:10 -0300

    Add parâmetro no decorator

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,7 +1,7 @@
 
 // Criando um decorator
 
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos:boolean = false) {
     return function(
         target: any, // Função construtor da classe de classes estatica, não estatíca retorna o prototype
         propertyKey: string, //  nome do método que foi decorado
@@ -10,11 +10,17 @@ export function logarTempoDeExecucao() {
         const metodoOriginal = descriptor.value; // guardando o método original
 
         descriptor.value = function (...args: Array<any>) { // modificando a função original
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos'
+            }
             const t1 = performance.now();
             // chamando método original
             const retorno = metodoOriginal.apply(this,args)
             const t2 = performance.now();
-            console.log(`MÉTODO: [${propertyKey}], tempo de execução: ${(t2 -t1)/1000} segundos!`)
+            console.log(`MÉTODO: [${propertyKey}], tempo de execução: ${(t2 -t1)/divisor} ${unidade}!`)
             retorno
         }
 
